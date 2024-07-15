fae18690b808b7c6e59b3b83699d8e85dff187de
AidenQian08 <aidenzqian@gmail.com>
2024-07-15 13:54:55 -0500

    Update inputs.py

--- inputs.py
+++ inputs.py
@@ -2,16 +2,24 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="Add your inputs here. Up to 10 files, core is required")
-    parser.add_argument('--core', type=str, required=True, help='This is the MANDATORY core table of the 4DN FISH-omics Format for Chromatin Tracing')
-    parser.add_argument('--rna', type=str, help="This table is used to store and share the results of RNA FISH-omics experiments, CONDITIONALLY REQUIRED")
-    parser.add_argument('--quality', type=str, help='This table is highly RECOMMENDED and it is designed to provide quality metrics for the Spot localization, information about the optical Channel, and various aberration corrections that have been applied prior to localization')
-    parser.add_argument('--bio', type=str, help="This table is highly RECOMMENDED and it is designed to store and share biological properties associated with individual Spots")
-    parser.add_argument('--demultiplexing', type=str, help='This table is OPTIONAL and is designed to be used in the case of multiplexed FISH experiments')
-    parser.add_argument('--trace', type=str, help='This OPTIONAL table is used to document properties that are globally associated with individual Traces')
-    parser.add_argument('--cell', type=str, help='This OPTIONAL table is used to document properties that are globally associated with individual Cells')
-    parser.add_argument('--subcell', type=str, help='This CONDITIONALLY REQUIRED table is used to document properties that are globally associated with individual sub-cellular ROIs that typically correspond to sub-nuclear features')
-    parser.add_argument('--extracell', type=str, help='This CONDITIONALLY REQUIRED table is used to document properties that are globally associated with individual extracellular structures')
-    parser.add_argument('--mapping', type=str, help='This table is used to provide the boundaries of Cells and other ROIs identified as part of this experiment. CONDITIONALLY REQUIRED if --subcell, -cell, and/or -extracell tables are provided')
+    
+    # categories of arguments
+    required = parser.add_argument_group('Required Arguments')
+    conditional = parser.add_argument_group('Conditionally Required Arguments')
+    recommended = parser.add_argument_group('Recommended Arguments')
+    optional = parser.add_argument_group('Optional Arguments')
+
+    # add the arguments
+    required.add_argument('--core', type=str, required=True, help='This is the MANDATORY core table of the 4DN FISH-omics Format for Chromatin Tracing')
+    conditional.add_argument('--rna', type=str, help="This table is used to store and share the results of RNA FISH-omics experiments, CONDITIONALLY REQUIRED")
+    recommended.add_argument('--quality', type=str, help='This table is highly RECOMMENDED and it is designed to provide quality metrics for the Spot localization, information about the optical Channel, and various aberration corrections that have been applied prior to localization')
+    recommended.add_argument('--bio', type=str, help="This table is highly RECOMMENDED and it is designed to store and share biological properties associated with individual Spots")
+    optional.add_argument('--demultiplexing', type=str, help='This table is OPTIONAL and is designed to be used in the case of multiplexed FISH experiments')
+    optional.add_argument('--trace', type=str, help='This OPTIONAL table is used to document properties that are globally associated with individual Traces')
+    optional.add_argument('--cell', type=str, help='This OPTIONAL table is used to document properties that are globally associated with individual Cells')
+    conditional.add_argument('--subcell', type=str, help='This CONDITIONALLY REQUIRED table is used to document properties that are globally associated with individual sub-cellular ROIs that typically correspond to sub-nuclear features')
+    conditional.add_argument('--extracell', type=str, help='This CONDITIONALLY REQUIRED table is used to document properties that are globally associated with individual extracellular structures')
+    conditional.add_argument('--mapping', type=str, help='This table is used to provide the boundaries of cells and other ROIs identified as part of this experiment. CONDITIONALLY REQUIRED if --subcell, -cell, and/or -extracell tables are provided')
 
     args = parser.parse_args()
 
