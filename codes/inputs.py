8065e66291c3a7e0f4847e692ab5efc49dfe9040
AidenQian08 <aidenzqian@gmail.com>
2024-07-19 08:56:15 -0500

    Update inputs.py

--- codes/inputs.py
+++ codes/inputs.py
@@ -1,26 +1,35 @@
 import argparse
+import os
+
+def csvfile_exists(file):
+    if not os.path.isfile(file):
+        return False
+    return True
+def add_to_input_dictionary(parser, key, file, dictionary):
+    if file is not None:
+        if not csvfile_exists(file):
+            parser.error(f"The file {file} does not exist")
+        else:
+            dictionary[key] = file
+            return dictionary
 
 def main():
-    parser = argparse.ArgumentParser(description="Add your inputs here. Up to 10 files, core is required")
-    
-    # categories of arguments
-    required = parser.add_argument_group('Required Arguments')
-    conditional = parser.add_argument_group('Conditionally Required Arguments')
-    recommended = parser.add_argument_group('Recommended Arguments')
-    optional = parser.add_argument_group('Optional Arguments')
+    parser = argparse.ArgumentParser(description="For more detailed help, go to [link]")
 
     # add the arguments
-    required.add_argument('--core', type=str, required=True, help='This is the MANDATORY core table of the 4DN FISH-omics Format for Chromatin Tracing')
-    conditional.add_argument('--rna', type=str, help="This table is used to store and share the results of RNA FISH-omics experiments, CONDITIONALLY REQUIRED")
-    recommended.add_argument('--quality', type=str, help='This table is highly RECOMMENDED and it is designed to provide quality metrics for the Spot localization, information about the optical Channel, and various aberration corrections that have been applied prior to localization')
-    recommended.add_argument('--bio', type=str, help="This table is highly RECOMMENDED and it is designed to store and share biological properties associated with individual Spots")
-    optional.add_argument('--demultiplexing', type=str, help='This table is OPTIONAL and is designed to be used in the case of multiplexed FISH experiments')
-    optional.add_argument('--trace', type=str, help='This OPTIONAL table is used to document properties that are globally associated with individual Traces')
-    optional.add_argument('--cell', type=str, help='This OPTIONAL table is used to document properties that are globally associated with individual Cells')
-    conditional.add_argument('--subcell', type=str, help='This CONDITIONALLY REQUIRED table is used to document properties that are globally associated with individual sub-cellular ROIs that typically correspond to sub-nuclear features')
-    conditional.add_argument('--extracell', type=str, help='This CONDITIONALLY REQUIRED table is used to document properties that are globally associated with individual extracellular structures')
-    conditional.add_argument('--mapping', type=str, help='This table is used to provide the boundaries of cells and other ROIs identified as part of this experiment. CONDITIONALLY REQUIRED if --subcell, --cell, and/or --extracell tables are provided')
+    parser.add_argument('--core', type=str, required=True, help='[path] This is the path input for the DNA-Spot/Trace Data core table')
+    parser.add_argument('--rna', type=str, help='[path] This is the path input for the RNA-Spot Data table')
+    parser.add_argument('--quality', type=str, help='[path] This is the path input for the Spot Quality table')
+    parser.add_argument('--bio', type=str, help='[path] This is the path input for the Spot Biological Data table')
+    parser.add_argument('--demultiplexing', type=str, help='[path] This is the path input for the Spot Demultiplexing table')
+    parser.add_argument('--trace', type=str, help='[path] This is the path input for the Trace Data table')
+    parser.add_argument('--cell', type=str, help='[path] This is the path input for the Cell Data table')
+    parser.add_argument('--subcell', type=str, help='[path] This is the path input for the Sub-Cell ROI Data table')
+    parser.add_argument('--extracell', type=str, help='[path] This is the path input for the Extra-Cell ROI Data table')
+    parser.add_argument('--mapping', type=str, help='[path] This is the path input for the Cell/ROI Mapping table')
     
+    parser.add_argument('--files', nargs='+', help='A list of files (optional)')
+
     args = parser.parse_args()
 
     # Conditional requirement checks
@@ -34,26 +43,21 @@ def main():
         parser.error("--mapping is required if --subcell, --extracell, or --cell is provided")
 
     # Collect the provided arguments into a dictionary
+    if not csvfile_exists(args.core):
+        parser.error(f"The file {args.core} does not exist")
     file_dictionary = {'core': args.core}
-    if args.rna is not None:
-        file_dictionary['rna'] = args.rna
-    if args.quality is not None:
-        file_dictionary['quality'] = args.quality
-    if args.bio is not None:
-        file_dictionary['bio'] = args.bio
-    if args.demultiplexing is not None:
-        file_dictionary['demultiplexing'] = args.demultiplexing
-    if args.trace is not None:
-        file_dictionary['trace'] = args.trace
-    if args.cell is not None:
-        file_dictionary['cell'] = args.cell
-    if args.subcell is not None:
-        file_dictionary['subcell'] = args.subcell
-    if args.extracell is not None:
-        file_dictionary['extracell'] = args.extracell
-    if args.mapping is not None:
-        file_dictionary['mapping'] = args.mapping
 
+    # Add the inputs to dictionary, as well as checking if the file path exists
+    add_to_input_dictionary(parser, 'rna', args.rna, file_dictionary)
+    add_to_input_dictionary(parser, 'quality', args.quality, file_dictionary)
+    add_to_input_dictionary(parser, 'bio', args.bio, file_dictionary)
+    add_to_input_dictionary(parser, 'demultiplexing', args.demultiplexing, file_dictionary)
+    add_to_input_dictionary(parser, 'trace', args.trace, file_dictionary)
+    add_to_input_dictionary(parser, 'cell', args.cell, file_dictionary)
+    add_to_input_dictionary(parser, 'subcell', args.subcell, file_dictionary)
+    add_to_input_dictionary(parser, 'extracell', args.extracell, file_dictionary)
+    add_to_input_dictionary(parser, 'mapping', args.mapping, file_dictionary)
+    
     print(file_dictionary)
 
 if __name__ == "__main__":
